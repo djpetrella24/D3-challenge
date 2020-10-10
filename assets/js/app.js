ab525ae13ecb977bc0cec24bc9e75554b248e536
Daniel Petrella <djpetrella24@gmail.com>
2020-10-10 19:05:17 -0400

    yay

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,40 @@
+var svgWidth = 1000;
+var svgHeight = 600;
+var margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50,
+};
+var height = svgHeight - margin.top - margin.bottom;
+var width = svgWidth - margin.left - margin.right;
+
+var svg = d3
+.select('#scatter')
+.append('svg')
+.attr('width', svgWidth)
+.attr('height', svgHeight); 
+
+// Append an SVG group
+var chartGroup = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .attr('class', 'chart');
+
+
+// set axes 
+var xAxis = 'poverty';
+var yAxis = 'healthcare';
+
 // Create link to csv
-d3.csv("./assets/data/data.csv").then(function(stateData) {
+d3.csv("./assets/data/data.csv").then(function(stateData, err) {
+    if (err) throw err;
     
-    console.log(stateData);
+    // parse data
+    stateData.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
+    })
+
 
 });
